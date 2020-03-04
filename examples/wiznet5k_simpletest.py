29eb7b5a2af6930e362d53c49a54c901e08d7ff8
brentru <robots199@me.com>
2020-03-04 09:58:19 -0500

    ESP32SPI_Simpletest-style-test

--- examples/wiznet5k_simpletest.py
+++ examples/wiznet5k_simpletest.py
@@ -1,10 +1,14 @@
-import time
-
 import board
 import busio
 import digitalio
+import adafruit_requests as requests
 from adafruit_wiznet5k.adafruit_wiznet5k import WIZNET5K
 import adafruit_wiznet5k.adafruit_wiznet5k_socket as socket
+
+print("Wiznet5k WebClient Test")
+
+TEXT_URL = "http://wifitest.adafruit.com/testwifi/index.html"
+JSON_URL = "http://api.coindesk.com/v1/bpi/currentprice/USD.json"
 
 cs = digitalio.DigitalInOut(board.D10)
 spi_bus = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
@@ -12,39 +16,29 @@ spi_bus = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 # Initialize ethernet interface with DHCP
 eth = WIZNET5K(spi_bus, cs)
 
-print("DHCP Assigned IP: ", eth.pretty_ip(eth.ip_address))
+# Initialize a requests object with a socket and ethernet interface
+requests.set_socket(socket, eth)
 
-socket.set_interface(eth)
+print("Chip Version:", eth.chip)
+print("MAC Address:", [hex(i) for i in eth.mac_address])
+print("My IP address is:", eth.pretty_ip(eth.ip_address))
+print("IP lookup adafruit.com: %s" %eth.pretty_ip(eth.get_host_by_name("adafruit.com")))
 
-host = 'wifitest.adafruit.com'
-port = 80
 
-addr_info = socket.getaddrinfo(host, port, 0, socket.SOCK_STREAM)
-sock = socket.socket(addr_info[0], addr_info[1], addr_info[2])
+#eth._debug = True
+print("Fetching text from", TEXT_URL)
+r = requests.get(TEXT_URL)
+print('-'*40)
+print(r.text)
+print('-'*40)
+r.close()
 
-sock.connect(addr_info[4])
+print()
+print("Fetching json from", JSON_URL)
+r = requests.get(JSON_URL)
+print('-'*40)
+print(r.json())
+print('-'*40)
+r.close()
 
-print("Connected to ", sock.getpeername())
-
-# Make a HTTP Request
-sock.send(b"GET /testwifi/index.html HTTP/1.1\n")
-sock.send(b"Host: 104.236.193.178\n")
-sock.send(b"Connection: close\n\n")
-
-# Start transmission timer
-start = time.monotonic()
-
-bytes_avail = 0
-while not bytes_avail:
-    bytes_avail = sock.available()
-    if bytes_avail > 0:
-        data = sock.recv(bytes_avail)
-        print(data[0])
-        break
-    time.sleep(0.05)
-
-end = time.monotonic()
-print("Received: %d bytes"%bytes_avail)
-end = end - start / 1000000.0
-rate = bytes_avail / end / 1000.0
-print("Rate = %0.5f kbytes/second"%rate)
+print("Done!")
