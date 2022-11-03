bca73d987f380bceacfb32a25a18a9859b9b0728
BiffoBear <martin@martinstehens.net>
2022-11-03 18:48:31 +0700

    Rewrote _parse_dns_response to handle CNAME records.

--- adafruit_wiznet5k/adafruit_wiznet5k_dns.py
+++ adafruit_wiznet5k/adafruit_wiznet5k_dns.py
@@ -3,6 +3,8 @@
 #
 # SPDX-License-Identifier: MIT
 
+# Each function only has one return statement. Pylint seems confused.
+# pylint: disable=inconsistent-return-statements
 """
 `adafruit_wiznet5k_dns`
 ================================================================================
@@ -47,7 +49,7 @@ def _debug_print(*, debug: bool, message: str) -> None:
         print(message)
 
 
-def _build_dns_query(domain: bytes) -> Tuple[int, bytearray]:
+def _build_dns_query(domain: bytes) -> Tuple[int, int, bytearray]:
     """Builds DNS header."""
     # generate a random, 16-bit, request identifier
     query_id = getrandbits(16)
@@ -76,124 +78,110 @@ def _build_dns_query(domain: bytes) -> Tuple[int, bytearray]:
         query += bytes(label, "utf-8")
     # Hard code null, question type and class as they never vary.
     query += bytearray([0x00, 0x00, 0x01, 0x00, 0x01])
-    return query_id, query
+    return query_id, len(query), query
 
 
-def _parse_dns_response(*, response: bytearray, query_id: int, debug) -> bytearray:
-    # pylint: disable=too-many-return-statements, too-many-branches, too-many-statements, too-many-locals
-    """Receives and parses DNS query response.
-    Returns desired hostname address if obtained, -1 otherwise.
-
+def _parse_dns_response(
+    *, response: bytearray, query_id: int, query_length: int, debug: bool
+) -> bytearray:
+    # pylint: disable=too-many-branches
     """
+    Parses a DNS query response.
 
+    :param bytearray response: Data returned as a DNS query response.
+    :param int query_id: The ID of the query that generated the response, used to validate
+        the response.
+    :param int query_length: The number of bytes in the DNS query that generated the response.
+    :param bool debug: Whether to output debugging messsages.
+
+    :returns bytearray: Four byte IPv4 address.
+
+    :raises ValueError: If the response does not yield a valid IPv4 address from a type A,
+        class IN answer.
+    """
     # Validate request identifier
-    xid = int.from_bytes(response[0:2], "big")
-    if not xid == query_id:
-        _debug_print(
-            debug=debug,
-            message="* DNS ERROR: Response ID {x:x} does not match query ID {y:x}".format(
-                x=xid, y=query_id
-            ),
+    response_id = int.from_bytes(response[0:2], "big")
+    if response_id != query_id:
+        raise ValueError(
+            "* DNS ERROR: Response ID {x:x} does not match query ID {y:x}".format(
+                x=response_id, y=query_id
+            )
         )
-        return -1
     # Validate flags
     flags = int.from_bytes(response[2:4], "big")
-    if not flags in (0x8180, 0x8580):
-        _debug_print(
-            debug=debug,
-            message="* DNS ERROR: Invalid flags 0x{x:x}, bx{x:b}".format(x=flags),
-        )
-        return -1
+    # Mask out authenticated, truncated and recursion bits, they don't stop us parsing.
+    flags &= 0xF87F
+    # Check that the response bit is set, the query is standard and no error occurred.
+    if flags != 0x8000:
+        raise ValueError("* DNS ERROR: Invalid flags 0x{x:x}, bx{x:b}.".format(x=flags))
     # Number of questions
-    qr_count = int.from_bytes(response[4:6], "big")
-    if not qr_count >= 1:
-        _debug_print(
-            debug=debug,
-            message="* DNS ERROR: Question count >=1, {}".format(qr_count),
+    question_count = int.from_bytes(response[4:6], "big")
+    # Never more than one question per DNS query in this implementation.
+    if question_count != 1:
+        raise ValueError(
+            "* DNS ERROR: Question count should be 1, is {}.".format(question_count)
         )
-        return -1
     # Number of answers
-    an_count = int.from_bytes(response[6:8], "big")
-    _debug_print(debug=debug, message="* DNS Answer Count: {}".format(an_count))
-    if not an_count >= 1:
-        return -1
-
-    # Parse query
-    ptr = 12
-    name_len = 1
-    while name_len > 0:
-        # read the length of the name
-        name_len = response[ptr]
-        if name_len == 0x00:
-            # we reached the end of this name
-            ptr += 1  # inc. pointer by 0x00
-            break
-        # advance pointer
-        ptr += name_len + 1
-
-    # Validate Query is Type A
-    q_type = int.from_bytes(response[ptr : ptr + 2], "big")
-    if not q_type == TYPE_A:
-        _debug_print(
-            debug=debug,
-            message="* DNS ERROR: Incorrect Query Type: {}".format(q_type),
+    answer_count = int.from_bytes(response[6:8], "big")
+    _debug_print(debug=debug, message="* DNS Answer Count: {}.".format(answer_count))
+    if answer_count < 1:
+        raise ValueError(
+            "* DNS ERROR: Answer count should be > 0, is {}.".format(answer_count)
         )
-        return -1
-    ptr += 2
 
-    # Validate Query is Type A
-    q_class = int.from_bytes(response[ptr : ptr + 2], "big")
-    if not q_class == TYPE_A:
-        _debug_print(
-            debug=debug,
-            message="* DNS ERROR: Incorrect Query Class: {}".format(q_class),
-        )
-        return -1
-    ptr += 2
-
-    # Let's take the first type-a answer
-    if response[ptr] != 0xC0:
-        return -1
-    ptr += 1
-
-    if response[ptr] != 0xC:
-        return -1
-    ptr += 1
-
-    # Validate Answer Type A
-    ans_type = int.from_bytes(response[ptr : ptr + 2], "big")
-    if not ans_type == TYPE_A:
-        _debug_print(
-            debug=debug,
-            message="* DNS ERROR: Incorrect Answer Type: {}".format(ans_type),
-        )
-        return -1
-    ptr += 2
-
-    # Validate Answer Class IN
-    ans_class = int.from_bytes(response[ptr : ptr + 2], "big")
-    if not ans_class == TYPE_A:
-        _debug_print(
-            debug=debug,
-            message="* DNS ERROR: Incorrect Answer Class: {}".format(ans_class),
-        )
-        return -1
-    ptr += 2
-
-    # skip over TTL
-    ptr += 4
-
-    # Validate addr is IPv4
-    data_len = int.from_bytes(response[ptr : ptr + 2], "big")
-    if not data_len == DATA_LEN:
-        _debug_print(
-            debug=debug,
-            message="* DNS ERROR: Unexpected Data Length: {}".format(data_len),
-        )
-        return -1
-    ptr += 2
-    # Return address
-    return response[ptr : ptr + 4]
+    # Parse answers
+    pointer = query_length  # Response header is the same length as the query header.
+    # pylint: disable=too-many-nested-blocks
+    try:
+        for answer in range(answer_count):
+            label_length = response[pointer]
+            if label_length == 0xC0:
+                # Pointer to the domain name, skip over it.
+                pointer += 2
+            else:
+                # Domain name, skip through it.
+                while label_length != 0x00:  # Null represents root of domain name
+                    pointer += label_length
+                    label_length = response[pointer]
+            # Check for a type A answer.
+            if int.from_bytes(response[pointer : pointer + 2], "big") == TYPE_A:
+                # Check for an IN class answer.
+                if int.from_bytes(response[pointer + 2 : pointer + 4], "big"):
+                    _debug_print(
+                        debug=debug,
+                        message="Type A, class IN found in answer {x} of {y}.".format(
+                            x=answer + 1, y=answer_count
+                        ),
+                    )
+                    # Set pointer to start of resource record.
+                    pointer += 8
+                    # Confirm that the resource record is 4 bytes (an IPv4 address).
+                    if int.from_bytes(response[pointer : pointer + 2], "big") == 4:
+                        ipv4 = response[pointer + 2 : pointer + 6]
+                        # Low probability that the response was truncated inside the 4 byte address.
+                        if len(ipv4) != 4:
+                            raise ValueError("IPv4 address is not 4 bytes.")
+                        _debug_print(
+                            debug=debug,
+                            message="IPv4 address found : 0x{:x}.".format(
+                                int.from_bytes(ipv4, "big")
+                            ),
+                        )
+                        return ipv4
+            # Set pointer to start of next answer
+            pointer += 10 + int.from_bytes(response[pointer + 8 : pointer + 10], "big")
+            _debug_print(
+                debug=debug,
+                message="Answer {x} of {y} was not type A, class IN.".format(
+                    x=answer + 1, y=answer_count
+                ),
+            )
+    except (IndexError, ValueError) as error:
+        # IndexError means we ran out of data in an answer, maybe truncated.
+        # ValueError means we ran out of answers.
+        raise ValueError(
+            "No type A, class IN answers found in the DNS response."
+        ) from error
 
 
 class DNS:
@@ -211,7 +199,8 @@ class DNS:
 
         self._dns_server = dns_address
         self._host = 0
-        self._request_id = 0  # request identifier
+        self._request_id = 0  # Request identifier.
+        self._request_length = 0  # Length of last query.
         self._pkt_buf = bytearray()
 
     def gethostbyname(self, hostname):
@@ -225,7 +214,9 @@ class DNS:
             return INVALID_SERVER
         self._host = hostname
         # build DNS request packet
-        self._request_id, self._pkt_buf = _build_dns_query(self._host)
+        self._request_id, self._request_length, self._pkt_buf = _build_dns_query(
+            self._host
+        )
 
         # Send DNS request packet
         self._sock.bind((None, DNS_PORT))
@@ -256,7 +247,10 @@ class DNS:
                 message="DNS Packet Received: {}".format(self._pkt_buf),
             )
             addr = _parse_dns_response(
-                response=self._pkt_buf, query_id=self._request_id, debug=self._debug
+                response=self._pkt_buf,
+                query_id=self._request_id,
+                query_length=self._request_length,
+                debug=self._debug,
             )
             if addr == -1:
                 _debug_print(
