c5208fb01bd37c93e692e30ca8568e6fad63d01f
Dan Jackson <dan@djackson.org>
2023-01-20 22:43:47 -0800

    Adding some print messages inside server loop

--- examples/wiznet5k_simpleserver.py
+++ examples/wiznet5k_simpleserver.py
@@ -29,9 +29,12 @@ server.bind((server_ip, server_port))  # Bind to IP and Port
 server.listen()  # Begin listening for incoming clients
 
 while True:
+    print(f"Accepting connections on {server_ip}:{server_port}")
     conn, addr = server.accept()  # Wait for a connection from a client.
+    print(f"Connection accepted from {addr}, reading exactly 1024 bytes from client")
     with conn:
         data = conn.recv(1024)
         if data:  # Wait for receiving data
             print(data)
             conn.send(data)  # Echo message back to client
+    print("Connection closed")
